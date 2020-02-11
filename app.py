f706fcbe85896738e609a63bcc3581267b1de70f
len2367 <vadimdendex@gmail.com>
2020-02-11 09:41:50 +0300

    testing heroku config_vars

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return os.environ("security_key")
 
 
 if __name__ == '__main__':
