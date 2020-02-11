b2a53fce2358943e0f9f66145dc1d88ef1081465
len2367 <vadimdendex@gmail.com>
2020-02-11 22:42:37 +0300

    _encrypt and _decrypt functions are ready

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from random import randint
 import os
+from cryptography.fernet import Fernet
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 app = Flask(__name__)
 security_key = os.environ.get("SECURITY_KEY")
@@ -8,7 +13,7 @@ security_key = os.environ.get("SECURITY_KEY")
 
 def _generate_key(length=32):
     """
-    This functions generates main key. Main key is used to encrypt note key and note id to one string
+    This functions generates key.
     :param `int` length:
     :return `str` main_key:
     """
@@ -18,13 +23,30 @@ def _generate_key(length=32):
         main_key += alphabet[randint(0, len(alphabet) - 1)]
     return main_key
 
-def _encrypt_text(text):
-    pass
+
+def _get_fermet_key(key):
+    salt = b"zo=6bLX1$SKMA$ZK+Kvhuf6yo\ZGy#&C"
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return base64.urlsafe_b64encode(kdf.derive(key.encode()))
+
+
+def _encrypt_text(key, text):
+    f = Fernet(_get_fermet_key(key))
+    return f.encrypt(text.encode()).decode()
+
+
+def _decrypt_text(key, text):
+    f = Fernet(_get_fermet_key(key))
+    return f.decrypt(text.encode()).decode()
+
 
 @app.route('/')
 def hello_world():
     return security_key
 
-
-if __name__ == '__main__':
-    app.run()
