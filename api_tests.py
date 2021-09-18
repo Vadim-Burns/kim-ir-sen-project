9350bf021a1badeb6a1cb031ff07f20b7be4223e
Vadim Burns <vadim.burns@protonmail.com>
2021-09-18 18:07:27 +0300

    splitting tests
    Signed-off-by: Vadim Burns <vadim.burns@protonmail.com>

--- api_tests.py
+++ api_tests.py
@@ -4,31 +4,159 @@ import requests
 
 
 class TestAPI(unittest.TestCase):
+    # TODO: автозапуск приложения при запуске тестов
     URL = "http://127.0.0.1:8080"
 
-    def test_api_correct(self):
+    """
+    Testing correct working
+    """
+
+    def test_create_note(self):
+        text = "Encrypting some weird text"
+
+        answer = requests.post(self.URL + "/api/add", json={"text": text})
+
+        self.assertTrue(
+            200 <= answer.status_code < 300,
+            "Incorrect answer status code after sending correct request for creating note"
+        )
+
+        self.assertTrue(
+            answer.json()["key"] is not None,
+            "No key after sending correct request for creating note"
+        )
+
+    def test_get_note(self):
+        text = "Encrypting some weird text"
+
+        # Create note
+        key = requests.post(self.URL + "/api/add", json={"text": text}).json().get("key")
+
+        # Get note
+        answer = requests.post(self.URL + "/api/get", json={"key": key})
+
+        self.assertTrue(
+            200 <= answer.status_code < 300,
+            "Incorrect answer status code after sending correct request for getting note"
+        )
+
+        self.assertTrue(
+            answer.json()["text"] is not None,
+            "No text after sending correct request for getting note"
+        )
+
+    def test_create_get_text_identical(self):
         text = "Encrypting some weird text"
 
         key = requests.post(self.URL + "/api/add", json={"text": text}).json().get("key")
-
         new_text = requests.post(self.URL + "/api/get", json={"key": key}).json().get("text")
+
         self.assertEqual(new_text, text, "Start and final texts are not identical")
 
-    def test_api_incorrect(self):
-        code = requests.post(self.URL + "/api/add").status_code
-        self.assertEqual(code, 400)
+    """
+    Testing error answers
+    """
 
-        code = requests.post(self.URL + "/api/add", json={"nothing": "nothing"}).status_code
-        self.assertEqual(code, 400)
+    def test_create_no_json_error(self):
+        answer = requests.post(self.URL + "/api/add")
 
-        code = requests.post(self.URL + "/api/get").status_code
-        self.assertEqual(code, 400)
+        self.assertEqual(
+            answer.status_code,
+            400,
+            "Wrong status code after sending post request to /api/add with empty body"
+        )
 
-        code = requests.post(self.URL + "/api/get", json={"nothing": "nothing"}).status_code
-        self.assertEqual(code, 400)
+        self.assertEqual(
+            answer.json()["error"],
+            "json data is not valid",
+            "Wrong error after sending post request to /api/add with empty body"
+        )
 
-        r = requests.post(self.URL + "/api/get", json={"key": "wrong"})
-        code = r.status_code
-        error = r.json().get("error")
-        self.assertEqual(error, "Wrong key")
-        self.assertEqual(code, 404)
+    def test_create_wrong_json_error(self):
+        answer = requests.post(self.URL + "/api/add", json={"nothing": "nothing"})
+
+        self.assertEqual(
+            answer.status_code,
+            400,
+            "Wrong status code after sending post request to /api/add with wrong json"
+        )
+
+        self.assertEqual(
+            answer.json()["error"],
+            "json data is not valid",
+            "Wrong error after sending post request to /api/add with wrong json"
+        )
+
+    def test_get_no_json_error(self):
+        answer = requests.post(self.URL + "/api/get")
+
+        self.assertEqual(
+            answer.status_code,
+            400,
+            "Wrong status code after sending post request to /api/get with empty body"
+        )
+
+        self.assertEqual(
+            answer.json()["error"],
+            "json data is not valid",
+            "Wrong error after sending post request to /api/get with empty body"
+        )
+
+    def test_get_wrong_json_error(self):
+        answer = requests.post(self.URL + "/api/get", json={"nothing": "nothing"})
+
+        self.assertEqual(
+            answer.status_code,
+            400,
+            "Wrong status code after sending post request to /api/add with wrong json"
+        )
+
+        self.assertEqual(
+            answer.json()["error"],
+            "json data is not valid",
+            "Wrong error after sending post request to /api/add with wrong json"
+        )
+
+    def test_get_wrong_key_error(self):
+        answer = requests.post(
+            self.URL + "/api/get",
+            json={
+                "key": "gAAAAABhRf19CDB9l8gw467nCR8DJ0Dr_qAWABNrsRdcbC_dMzFKISA_VKaowQq-T7fLAka4JJp5JujmBTiPACRH7KfLuOcrBBhMOecBhT1R-tOpGdUpsT2tKhRVem6587GIHdBDkasI"
+            }
+        )
+
+        self.assertEqual(
+            answer.status_code,
+            400,
+            "Wrong status code after sending post request to /api/add with wrong key"
+        )
+
+        self.assertEqual(
+            answer.json()["error"],
+            "invalid key",
+            "Wrong error after sending post request to /api/add with wrong key"
+        )
+
+    def test_note_not_found_error(self):
+        text = "Encrypting some weird text"
+
+        # Create note
+        key = requests.post(self.URL + "/api/add", json={"text": text}).json().get("key")
+
+        # Delete note from database
+        requests.post(self.URL + "/api/get", json={"key": key})
+
+        # Request that note again
+        answer = requests.post(self.URL + "/api/get", json={"key": key})
+
+        self.assertEqual(
+            answer.status_code,
+            404,
+            "Wrong status code after sending deleted note's key to /api/get"
+        )
+
+        self.assertEqual(
+            answer.json()["error"],
+            "note not found",
+            "Wrong error after sending deleted note's key to /api/get"
+        )
--- app.py
+++ app.py
@@ -183,6 +183,5 @@ def get():
     }
 
 
-# TODO: включить прод версию
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
